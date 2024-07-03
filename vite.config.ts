71aa5ba6253aed615ce9a9602147f620fb2c5fea
Saina <sainakey5@gmail.com>
2024-07-03 23:47:26 +0900

    チュートリアル途中

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [
     remix({
+      ssr: true,
       ignoredRouteFiles: ["**/*.css"],
     }),
     tsconfigPaths(),
